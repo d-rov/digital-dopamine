37a3dcc1e0aeb7fcbd5f8758dd51a257e5ad9aaa
duncan-ro <duncan.rov@gmail.com>
2025-05-30 22:31:15 -0600

    adding postSiteClassification to frontend api

--- extension/src/api.ts
+++ extension/src/api.ts
@@ -1,18 +1,32 @@
 import type { Classification, LogEntry } from "./types";
 import { API_BASE_URL } from "./config.js";
 
+// POST
+
 export const postLogEntry = async (
   data: LogEntry
-): Promise<{ status: number; data: LogEntry[] }> => {
+): Promise<{ status: number }> => {
   // post a log entry to the backend
   const res = await fetch(`${API_BASE_URL}/log`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(data),
   });
-  const json: LogEntry[] = await res.json();
-  return { status: res.status, data: json };
+  return { status: res.status };
 };
+
+export const postSiteClassification = async (
+  data: Classification
+): Promise<{ status: number }> => {
+  const res = await fetch(`${API_BASE_URL}/classify`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data),
+  });
+  return { status: res.status };
+};
+
+// GET
 
 export const getSiteClassification = async (
   data: string
--- extension/src/background.ts
+++ extension/src/background.ts
@@ -10,7 +10,7 @@ chrome.tabs.onActivated.addListener(async (activeInfo) => {
   try {
     const tab = await chrome.tabs.get(activeInfo.tabId);
     if (!tab || !tab.url || !tab.id) return;
-    // just get URL root for database interactions
+    // need to sanitize for restricted URLs such as chrome://
     const parsed = new URL(tab.url);
     const hostUrl = parsed.host;
 
@@ -46,42 +46,6 @@ chrome.tabs.onActivated.addListener(async (activeInfo) => {
   } catch (err) {
     console.error("Error in onActivated listener: ", err);
   }
-
-  // chrome.tabs.get(activeInfo.tabId, (tab) => {
-  //   if (!tab || !tab.url || !tab.id) return;
-
-  //   console.log(tab.url); // TESTING
-  //   console.log(tab.id); // TESTING
-  //   const parsed = new URL(tab.url);
-  //   console.log(parsed); // TESTING
-  //   const hostUrl = parsed.hostname;
-  //   console.log(hostUrl); // TESTING
-  //   // check if currUrl does != tab.id and stop previous time tracker
-  //   // set new currUrl to tab.url
-  //   if (currUrl != "" && currUrl !== hostUrl) {
-  //     const tracker = trackerMap.get(currUrl);
-  //     if (!tracker) return "TimeTracker object not found";
-  //     const tabData = tracker.stop();
-  //     // do I want to add a line into the db here?
-  //     console.log(tabData); // TESTING
-  //     const retVal = postLogEntry(tabData);
-  //     // lets do a little error checking with retVal
-  //     console.log("retVal: ", retVal); // TESTING
-  //   }
-
-  //   currUrl = hostUrl;
-  //   console.log(`Current tab id: ${currUrl}`); // TESTING
-
-  //   if (trackerMap.has(hostUrl)) {
-  //     const tracker = trackerMap.get(hostUrl);
-  //     if (!tracker) return "TimeTracker object not found";
-  //     tracker.start(hostUrl);
-  //   } else if (!trackerMap.has(hostUrl)) {
-  //     const timeTracker = new TimeTracker();
-  //     timeTracker.start(hostUrl);
-  //     trackerMap.set(hostUrl, timeTracker);
-  //   }
-  // });
 });
 
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
